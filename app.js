3ade8ac5177c41686cc604714dca467560693041
JY <smashdownn@gmail.com>
2017-08-09 10:59:03 +0700

    external link test

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var deeplink = require('./routes/deeplink');
 var users = require('./routes/users');
 
 var app = express();
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/deeplink', deeplink);
 app.use('/users', users);
 
 // catch 404 and forward to error handler

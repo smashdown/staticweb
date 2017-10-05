fb5054c7fce9d94c5cf8b24b76cb49dee2fea3a1
Jongyoung Jeong <smashdownn@gmail.com>
2017-10-05 15:59:05 +0700

    football

--- app.js
+++ app.js
@@ -16,6 +16,35 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.all('*', function(req, res,next) {
+  /**
+   * Response settings
+   * @type {Object}
+   */
+  var responseSettings = {
+      "AccessControlAllowOrigin": req.headers.origin,
+      "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+      "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+      "AccessControlAllowCredentials": true
+  };
+
+  /**
+   * Headers
+   */
+  res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+  res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+  res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+  res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+  if ('OPTIONS' == req.method) {
+      res.send(200);
+  }
+  else {
+      next();
+  }
+
+
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
